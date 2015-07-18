9f963e63b2a8770f1c8d81c4c46d242b4a653962
Kristina Twilt <asnowyevening@gmail.com>
2015-07-18 18:07:33 -0400

    localize image carousel changes

--- js/thing.js
+++ js/thing.js
@@ -1,22 +1,22 @@
 var imgArray = new Array();
 
 imgArray[0] = new Image();
-imgArray[0].src = 'http://192.168.39.88:8080/img/1.jpg';
+imgArray[0].src = '../img/1.jpg';
 
 imgArray[1] = new Image();
-imgArray[1].src = 'http://192.168.39.88:8080/img/2.jpg';
+imgArray[1].src = '../img/2.jpg';
 
 imgArray[2] = new Image();
-imgArray[2].src = 'http://192.168.39.88:8080/img/3.jpg';
+imgArray[2].src = '../img/3.jpg';
 
 imgArray[3] = new Image();
-imgArray[3].src = 'http://192.168.39.88:8080/img/4.jpg';
+imgArray[3].src = '../img/4.jpg';
 
 imgArray[4] = new Image();
-imgArray[4].src = 'http://192.168.39.88:8080/img/5.jpg';
+imgArray[4].src = '../img/5.jpg';
 
 imgArray[5] = new Image();
-imgArray[5].src = 'http://192.168.39.88:8080/img/6.jpg';
+imgArray[5].src = '../img/6.jpg';
 
 $("#nextImage").click(function () {
 	var img = $('#dynamic')[0];
